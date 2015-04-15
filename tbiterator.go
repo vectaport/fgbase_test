16b4501d92722f3684ecebd5543f099b85d44d45
Scott Johnston <johnston@vectaport.com>
2015-04-15 15:47:54 -0500

    Use funceither

--- tbiterator.go
+++ tbiterator.go
@@ -2,54 +2,37 @@ package main
 
 import (
 	"github.com/vectaport/flowgraph"
+	"math/rand"
 	"time"
 )
 
 func tbi(x flowgraph.Edge) {
 
-	node := flowgraph.MakeNode("tbi", nil, []*flowgraph.Edge{&x}, nil, nil)
-
-	x.Aux = 10
-
-	var i int = 0
-	for {
-		if (i>1000000) { break }
-
-		if node.RdyAll(){
-			x.Val = x.Aux
-			x.Aux = x.Aux.(int) + 1
-			node.SendAll()
-			i = i + 1
-		}
-
-		node.RecvOne()
-
-	}
+	node := flowgraph.MakeNode("tbi", nil, []*flowgraph.Edge{&x}, nil,
+		func(n *flowgraph.Node) { n.Dsts[0].Val = rand.Intn(15)+1 })
+	node.Run()
 }
-
 
 func main() {
 
 	flowgraph.TraceLevel = flowgraph.V
 	flowgraph.TraceIndent = false
 
-	e0 := flowgraph.MakeEdge("e0",nil)
-	e1 := flowgraph.MakeEdge("e1",nil)
-	e2 := flowgraph.MakeEdge("e2",nil)
-	e3 := flowgraph.MakeEdge("e3",nil)
-	e4 := flowgraph.MakeEdge("e4",nil)
-	e5 := flowgraph.MakeEdge("e5",int(0))
-	e6 := flowgraph.MakeEdge("e6",nil)
+	e0 := flowgraph.MakeEdge("e0", nil)
+	e1 := flowgraph.MakeEdge("e1", nil)
+	e2 := flowgraph.MakeEdge("e2", nil)
+	e3 := flowgraph.MakeEdgeConst("e3", 1)
+	e4 := flowgraph.MakeEdge("e4", nil)
+	e5 := flowgraph.MakeEdge("e5", int(0))
+	e6 := flowgraph.MakeEdge("e6", nil)
 
 	go tbi(e0)
 	go flowgraph.FuncRdy(e0, e5, e1)
-        go flowgraph.FuncArbit(e1, e6, e2)
+	go flowgraph.FuncArbit(e1, e6, e2)
 	go flowgraph.FuncSub(e2, e3, e4)
 	go flowgraph.FuncSteerc(e4, e5, e6)
-	go flowgraph.FuncConst(e3,1)
 
 	time.Sleep(time.Second)
 	flowgraph.StdoutLog.Printf("\n")
 
 }
-
