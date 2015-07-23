994d1e5a4291477c106a342f0ac6b41b4fc6a5f6
Scott Johnston <johnston@vectaport.com>
2015-07-22 18:16:27 -0700

    full map/reduce example

--- tbreduce.go
+++ tbreduce.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"math/rand"
+	"time"
 
 	"github.com/vectaport/flowgraph"
 )
@@ -24,6 +25,7 @@ func tbi(x flowgraph.Edge) flowgraph.Node {
 	x.Ack = make(chan flowgraph.Nada, flowgraph.ChannelSize)
 	node := flowgraph.MakeNode("tbi", nil, []*flowgraph.Edge{&x}, nil, 
 		func (n *flowgraph.Node) { 
+			time.Sleep(time.Second/100000)
 			l:=len((*x.Data)[0])
 			if MaxChanLen < l {
 				MaxChanLen = l
@@ -32,29 +34,68 @@ func tbi(x flowgraph.Edge) flowgraph.Node {
 	return node
 }
 
-var reduceHz []float64
-var reduceBase int64
+var tboHz []float64
+var tboBase int64
 
 func tbo(a flowgraph.Edge) flowgraph.Node {
 
 	node := flowgraph.MakeNode("tbo", []*flowgraph.Edge{&a}, nil, nil, 
 		func (n *flowgraph.Node) { 
-			if n.Cnt%1000==0 {
-				reduceHz[n.ID-reduceBase] = float64(n.Cnt)/flowgraph.TimeSinceStart()
+			if n.Cnt%100==0 {
+				tboHz[n.ID-tboBase] = float64(n.Cnt)/flowgraph.TimeSinceStart()
 			}})
 	return node
 }
+
+func testOrder(n *flowgraph.Node, dict []string) {
+	for i := 0; i<len(dict)-1; i++ {
+		if dict[i]>=dict[i+1] {
+			n.LogError("out of order %v\n", dict)
+		}
+	}
+}
+
+func reducer(n *flowgraph.Node, s, d flowgraph.Datum) flowgraph.Datum {
+	dict := d.([]string)
+	ss := s.(string)
+	lo := 0
+	hi := len(dict)-1
+	for lo<=hi {
+		mid := (lo+hi)/2
+		if ss < dict[mid] {
+			hi = mid-1
+		} else if ss > dict[mid] {
+			lo = mid+1
+		} else {
+			break	
+		}
+	}
+	i := lo
+	
+	if i==len(dict) {
+		dict = append(dict, ss)
+	} else {
+		dict = append(dict, "")
+		copy(dict[i+1:], dict[i:])
+		dict[i] = ss
+
+		testOrder(n, dict)
+	}
+	return dict
+	
+}
+
 
 func main() {
 
 	
 	nreducep := flag.Int("nreduce", 26, "number of reducers")
-	nmapp := flag.Int("nmap", 6, "number of mappers")
-	flowgraph.ConfigByFlag(map[string]interface{}{ "ncore":3, "chansz":32768, "trace": "Q", "sec":4})
+	nmapp := flag.Int("nmap", 128, "number of mappers")
+	flowgraph.ConfigByFlag(map[string]interface{}{ "ncore":6, "chansz":128, "trace": "Q", "sec":4})
 	nreduce := *nreducep
 	nmap := *nmapp
 
-	e,n := flowgraph.MakeGraph(nmap+nreduce,nmap*2+nreduce)
+	e,n := flowgraph.MakeGraph(nmap+nreduce*2,nmap*2+nreduce*2)
 
 
 	for i:= 0; i<nmap; i++ {
@@ -70,21 +111,25 @@ func main() {
 		}
 		return -1
 	}
+
 	p := flowgraph.FuncMap(e[0:nmap], e[nmap:nmap+nreduce], mapSel)
 	copy(n[nmap:2*nmap], p.Nodes())
-
+	
 	for i:= 0; i<nreduce; i++ {
-		n[2*nmap+i] = tbo(e[nmap+i])
+		n[2*nmap+i] = flowgraph.FuncReduce(e[nmap+i], e[nmap+nreduce+i], reducer)
+	}
+	for i:= 0; i<nreduce; i++ {
+		n[2*nmap+nreduce+i] = tbo(e[nmap+nreduce+i])
 	}
 
-	reduceBase = int64(2*nmap)
-	reduceHz = make([]float64, nreduce)
+	tboBase = int64(2*nmap+nreduce)
+	tboHz = make([]float64, nreduce)
 
 	flowgraph.RunAll(n)
 
 	sum := 0.0
-	for i:=0; i<len(reduceHz); i++ {
-		sum += reduceHz[i]
+	for i:=0; i<len(tboHz); i++ {
+		sum += tboHz[i]
 	}
 	if sum>1000*1000 {
 		flowgraph.StdoutLog.Printf("%.2f Mhz\n", sum/1000/1000)
