8ffb7222604dbc0fd15f09844255ae5f4a2124fc
Scott Johnston <johnston@vectaport.com>
2015-08-20 16:48:19 -0500

    new tbrsh test bench ??

---
+++ tbrsh.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"math"
+
+	"github.com/vectaport/flowgraph"
+)
+
+func tbi(x, y flowgraph.Edge) flowgraph.Node {
+
+	node := flowgraph.MakeNode("tbi", nil, []*flowgraph.Edge{&x, &y}, nil, nil)
+	node.RunFunc = tbiRun
+	return node
+}
+
+func tbiRun (n *flowgraph.Node) {
+	x := n.Dsts[0]
+	y := n.Dsts[1]
+
+	var i uint = 0
+	for {
+		if (i>10) { break }
+		if n.RdyAll(){
+			x.Val = i
+			y.Val = uint(2)
+			n.SendAll()
+			i = i + 1
+		}
+		n.RecvOne()
+	}
+
+	i = 0
+	for {
+		if (i>9) { break }
+		if n.RdyAll(){
+			x.Val = uint(i)
+			y.Val = uint(3)
+			n.SendAll()
+			i = i + 1
+		}
+		n.RecvOne()
+	}
+
+	var xv interface{} = uint64(math.MaxUint64)
+	var yv interface{} = uint64(1)
+	i = 0
+	for {
+		if (i > 0) { break }
+		if n.RdyAll(){
+			x.Val = xv
+			y.Val = yv
+			n.SendAll()
+			i = i + 1
+		}
+		n.RecvOne()
+	}
+
+	xv = uint8(0)
+	yv = uint(0)
+	i = 0
+	for  {
+		if (i > 0) { break }
+		if n.RdyAll() {
+			x.Val = xv
+			y.Val = yv
+			n.SendAll()
+			i = i + 1
+		}
+		n.RecvOne()
+	}
+
+	xv = uint8(0)
+	yv = uint(0)
+	i = 0
+	for  {
+		if (i > 0) { break }
+		if n.RdyAll() {
+			x.Val = xv
+			y.Val = yv
+			n.SendAll()
+			i = i + 1
+		}
+		n.RecvOne()
+	}
+
+
+	xv = "Can you right shift a string by an int?"
+	yv = uint8(77)
+	i = 0
+	for  {
+		if (i > 0) { break }
+		if n.RdyAll() {
+			x.Val = xv
+			y.Val = yv
+			n.SendAll()
+			i = i + 1
+		}
+		n.RecvOne()
+	}
+
+	xv = [4]complex128 {0+0i,0+0i,0+0i,0+0i}
+	yv = uint8(77)
+	i = 0
+	for  {
+		if (i > 0) { break }
+		if n.RdyAll() {
+			x.Val = xv
+			y.Val = yv
+			n.SendAll()
+			i = i + 1
+		}
+		n.RecvOne()
+	}
+	
+
+	// read all the acks to clean up
+	for  {
+		n.RecvOne()
+	}
+	
+
+}
+
+func tbo(a flowgraph.Edge) flowgraph.Node {
+
+	node := flowgraph.MakeNode("tbo", []*flowgraph.Edge{&a}, nil, nil, nil)
+	return node
+
+}
+
+func main() {
+
+	flowgraph.ConfigByFlag(nil)
+
+	e,n := flowgraph.MakeGraph(3,3)
+
+	n[0] = tbi(e[0], e[1])
+	n[1] = flowgraph.FuncRsh(e[0], e[1], e[2])
+	n[2] = tbo(e[2])
+
+	flowgraph.RunAll(n)
+
+}
+
