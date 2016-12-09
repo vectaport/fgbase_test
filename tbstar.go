d343b926a6f47bccd5fffede7b62fdfbc42a906a
Scott Johnston <johnston@vectaport.com>
2016-12-09 10:54:08 -0800

    workin on star

--- tbstar.go
+++ tbstar.go
@@ -16,54 +16,99 @@ var teststrings = []string{
 	"",
 }
 
-func tbi(x flowgraph.Edge) flowgraph.Node {
+func tbi(dnstreq flowgraph.Edge, newmatch flowgraph.Edge) flowgraph.Node {
 
         i := 0
 
-	node := flowgraph.MakeNode("tbi", nil, []*flowgraph.Edge{&x},
+	Prev:=make(map[string]string)
+
+	node := flowgraph.MakeNode("tbi", []*flowgraph.Edge{&dnstreq}, []*flowgraph.Edge{&newmatch},
 		func (n *flowgraph.Node) bool {
-			return i<=len(teststrings) && n.DefaultRdyFunc()
+			return dnstreq.SrcRdy(n) || newmatch.DstRdy(n) && i<len(teststrings)
 		},
-		func (n *flowgraph.Node) { 
+		func (n *flowgraph.Node) {
+			if dnstreq.SrcRdy(n) {
+				match := dnstreq.Val.(regexp.Search)
+				match.Curr = Prev[match.Orig][1:]
+				Prev[match.Orig] = match.Curr
+				newmatch.Val = match
+				return
+			}
+			dnstreq.NoOut = true
                         if i<len(teststrings) {
-				x.Val = regexp.Regexp{Orig:teststrings[i], Curr:teststrings[i]}
+				newmatch.Val = regexp.Search{Orig:teststrings[i], Curr:teststrings[i], State:regexp.Live}
                         } else {
 				if i==len(teststrings) {
-					x.Val = nil
+					newmatch.Val = regexp.Search{}
 				} else {
-					x.NoOut = false
+					newmatch.NoOut = true
 				}
                         }
                         i++
+			dnstreq.NoOut = true
 		})
 	return node
 
 }
 
-func tbo(a flowgraph.Edge) flowgraph.Node {
+func tbo(oldmatch flowgraph.Edge, dnstreq flowgraph.Edge) flowgraph.Node {
 
-	node := flowgraph.MakeNode("tbo", []*flowgraph.Edge{&a}, nil, nil, nil)
+	node := flowgraph.MakeNode("tbo", []*flowgraph.Edge{&oldmatch}, []*flowgraph.Edge{&dnstreq}, nil,
+		func (n *flowgraph.Node) {
+			dnstreq.Val = regexp.Search{} // echo back
+		})
 	return node
          
 }
 
-func main() {
+
+type edgeCnt int
+const (
+	newmatch edgeCnt = iota
+	subsrc
+	dnstreq
+	oldmatch
+	subdst
+	upstreq
+	apples
+	oranges
+	applesmatch
+	orangesmatch
+	edgenum
+)
+
+var edgeNames []string = []string {
+	"newmatch",
+	"subsrc",
+	"dnstreq",
+	"oldmatch",
+	"subdst",
+	"upstreq",
+	"apples",
+	"oranges",
+	"applesmatch",
+	"orangesmatch",
+}
 
 	
+func main() {
+
 	flowgraph.ConfigByFlag(nil)
 
-	e,n := flowgraph.MakeGraph(8, 6)
+	e,n := flowgraph.MakeGraph(int(edgenum), 6)
+	flowgraph.NameEdges(e,edgeNames)
 
-	e[6].Const("apples")
-	e[7].Const("oranges")
+
+	e[apples].Const("apples")
+	e[oranges].Const("oranges")
 
 	// (apples|oranges)*
-	n[0] = tbi(e[0])
-        n[1] = regexp.FuncStar(e[0], e[4], e[1], e[5])
-	n[2] = regexp.FuncMatch(e[1], e[6], e[2])
-	n[3] = regexp.FuncMatch(e[1], e[7], e[3])
-        n[4] = regexp.FuncBar(e[2], e[3], e[4], false)
-        n[5] = tbo(e[5])
+	n[0] = tbi(e[upstreq], e[newmatch])
+        n[1] = regexp.FuncStar(e[newmatch], e[subsrc], e[dnstreq], e[oldmatch], e[subdst], e[upstreq])
+	n[2] = regexp.FuncMatch(e[subdst], e[apples], e[applesmatch])
+	n[3] = regexp.FuncMatch(e[subdst], e[oranges], e[orangesmatch])
+        n[4] = regexp.FuncBar(e[applesmatch], e[orangesmatch], e[subsrc], false)
+        n[5] = tbo(e[oldmatch], e[dnstreq])
 
 	flowgraph.RunAll(n)
 
