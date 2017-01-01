c2dfab17e9978f773c2020dab0eaa81011b69bfd
Scott Johnston <johnston@vectaport.com>
2017-01-01 11:07:45 -0800

    working on Flow use

--- tbserver.go
+++ tbserver.go
@@ -14,6 +14,7 @@ func tbo(a flowgraph.Edge) flowgraph.Node {
 	node := flowgraph.MakeNode("tbo", []*flowgraph.Edge{&a}, nil, nil, 
 		func (n *flowgraph.Node) {
 			// time.Sleep(time.Duration(rand.Intn(150000)))
+			a.Flow = true
 			if n.Cnt%10000==0 {
 				flowgraph.StdoutLog.Printf("%.2f: %d (%.2f hz)\n", flowgraph.TimeSinceStart(), n.Cnt, float64(n.Cnt)/flowgraph.TimeSinceStart())
 			}
